b11747e64cd9cd1c41678e47acc48dc066956c8b
Dima <taominhquan2004@gmail.com>
2025-05-04 11:15:42 +0200

    changed the position of folder pages

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,9 +2,9 @@ import './App.css'
 import '../../styles/reset.css'
 import MainMenu from '../MainMenu/MainMenu'
 import { Routes, Route } from 'react-router-dom'
-import HomePage from '../pages/HomePage/HomePage'
+import HomePage from '../../pages/HomePage/HomePage'
 import Articles from '../Articles/Articles'
-import SingleArticlePage from '../pages/SingleArticlePage/SingleArticlePage'
+import SingleArticlePage from '../../pages/SingleArticlePage/SingleArticlePage'
 function App() {
   return (
     <>
